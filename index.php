ff1920ea09bb6ea526c21643ace776094f4beeec
shorfana <32379054+shorfana@users.noreply.github.com>
2019-10-10 11:11:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas 1 101164xx 101164xx 101164xx 101164xx 101164xx</title>
+	<link href="css.css" rel="stylesheet">
+</head>
+<body>
+	<main>
+	
+	<div class="ui-wrapper">
+		<div class="ui-searchbar">
+			<h1>Tugas 1</h1>
+		</div>
+
+		<div class="ui-topsites">
+			<a href="#" class="ui-topsites-item">
+				<i class="fas fa-globe ui-topsite-icon"></i>
+				<span>Erzi Hutama Dwiarama Putra</span>
+				<span class="ui-topsites-item-edit"><i class="fas fa-ellipsis-v"></i></span>
+			</a>
+
+			<a href="#" class="ui-topsites-item">
+				<i class="fas fa-globe ui-topsite-icon"></i>
+				<span>Muhammad Iqbal Shorfana</span>
+				<span class="ui-topsites-item-edit"><i class="fas fa-ellipsis-v"></i></span>
+			</a>
+			
+
+			<a href="#" class="ui-topsites-item">
+				<i class="fas fa-globe ui-topsite-icon"></i>
+				<span>Randi Putra Utama</span>
+				<span class="ui-topsites-item-edit"><i class="fas fa-ellipsis-v"></i></span>
+			</a><br><br>
+
+			<a href="#" class="ui-topsites-item">
+				<i class="fas fa-globe ui-topsite-icon"></i>
+				<span>Adam Jordy</span>
+				<span class="ui-topsites-item-edit"><i class="fas fa-ellipsis-v"></i></span>
+			</a>
+
+			<a href="#" class="ui-topsites-item">
+				<i class="fas fa-globe ui-topsite-icon"></i>
+				<span>Faldi Favian</span>
+				<span class="ui-topsites-item-edit"><i class="fas fa-ellipsis-v"></i></span>
+			</a>
+
+					<a href="#" class="ui-topsites-item">
+				<i class="fas fa-user ui-topsite-icon"></i>
+				<span>Riview & Security</span>
+
+				<span class="ui-topsites-item-edit"><i class="fas fa-ellipsis-v"></i></span>
+			</a>
+			
+		</div>
+		
+	</div>
+	
+</main>
+</body>
+</html>
